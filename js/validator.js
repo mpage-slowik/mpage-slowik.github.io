1f41ef605be951d7d17757d62d8408c35ab6eefe
Max <mpage-sowik@outlook.com>
2019-01-03 00:21:30 -0500

    fixed typo

---
+++ js/validator.js
@@ -0,0 +1,12 @@
+ function FormValidation(){
+    var name = document.forms["quoteForm"]["name"];               
+    var email = document.forms["quoteForm"]["email"];    
+    var phone = document.forms["quoteForm"]["phone"];  
+    var what =  document.forms["quoteForm"]["postalCode"];  
+    var password = document.forms["quoteForm"]["purchasingOption"];  
+    var address = document.forms["quoteForm"]["rooms"];
+    if(name == ""){
+        
+    }
+
+}
